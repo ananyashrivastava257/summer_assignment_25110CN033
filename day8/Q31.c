723dc90898cada60b2a3bb81888990054f40acd5
ananyashrivastava257 <shrivananya257@gmail.com>
2026-06-09 13:21:33 +0530

    Add files via upload

---
+++ day8/Q31.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i, j;
+    printf("enter number of rows:");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    {
+        char ch = 'A';   
+        for(j=1;j<=i;j++)
+           {printf("%C",ch);
+            ch++;}
+     printf("\n");
+    }
+    return 0;
+}
