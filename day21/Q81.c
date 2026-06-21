733510825a1a7f0bb58ba733a8924a7176192674
ananyashrivastava257 <shrivananya257@gmail.com>
2026-06-21 22:28:38 +0530

    Create Q81.c

---
+++ day21/Q81.c
@@ -0,0 +1,21 @@
+// Write a program to Find string length without strlen().
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int length = 0;
+    printf("Enter a string : ");
+    fgets(str, sizeof(str), stdin);
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '\n') {
+            str[i] = '\0';
+            break;
+        }
+    }
+    while (str[length] != '\0') {
+        length++;
+    }
+    printf("Length of the string = %d\n", length);
+    return 0;
+}
